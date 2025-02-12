55afc72215d12c8a5de03b68ab28e99d467f8dc2
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-12 09:57:16 -0800

    winner check commit

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,22 +13,41 @@ def board(spots):
 def spot_input():
     user_input = [str(input("Pick a spot to place your mark: ")),str.upper(input("What mark are you: "),)]
     return user_input
-def winner_check():
-    pass
+
+def winner_check(spots,scores):
+    #check top
+    for i in range(len(spots),3):
+        if spots[i] == spots[i+1] == spots[i+2]:
+            if spots[i] == "X":
+                scores['Player 1'] +=1
+                return (False,"X WINS")
+            else:
+                scores['Player 2'] +=1
+                return (False,"O WINS")
+    return (True, "KEEP PLAYING")
+    #check middle
+
+    #check bottom
+
+    #check rows
+
+    #check diagonals
+
+
 def main():
     board_spaces = [i for i in range(1,10)]
-    board(board_spaces)
     play = True
+    #Player 1 = X Player 2 = O
     scores = {'Player 1':0,'Player 2':0}
     while play:
+        board(board_spaces)
         user_spot = spot_input()
         #puts user input into correct spot
         for i in board_spaces:
             if str(i) == user_spot[0]:
                 board_spaces[i-1]=user_spot[1]
-        if(winner_check):
-            play = False
-        print(f"Player 1 score: {scores['Player 1']} \nPlayer 2 score: {scores['Player 2']}")
+        play,win_message = winner_check(board_spaces,scores)
+        print(f"Player 1 score: {scores['Player 1']} \nPlayer 2 score: {scores['Player 2']} \n{win_message}")
 
 if __name__ == "__main__":
     main()
