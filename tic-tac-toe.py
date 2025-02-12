6133feef96beb800b9454275439b46514396bd86
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-02-12 09:53:44 -0800

    winner_check

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,22 +13,30 @@ def board(spots):
 def spot_input():
     user_input = [str(input("Pick a spot to place your mark: ")),str.upper(input("What mark are you: "),)]
     return user_input
-def winner_check():
-    pass
+def winner_check(spots):
+    if spots[0] == spots[4] == spots[8] == "O":
+        return (False, "O wins")
+    if spots[2] == spots[4] == spots[8] == "O":
+        return (False, "O wins")
+    else:
+        return True
+    
 def main():
     board_spaces = [i for i in range(1,10)]
-    board(board_spaces)
     play = True
     scores = {'Player 1':0,'Player 2':0}
     while play:
+        board(board_spaces)
         user_spot = spot_input()
         #puts user input into correct spot
         for i in board_spaces:
             if str(i) == user_spot[0]:
                 board_spaces[i-1]=user_spot[1]
-        if(winner_check):
-            play = False
         print(f"Player 1 score: {scores['Player 1']} \nPlayer 2 score: {scores['Player 2']}")
+        play, win_state = winner_check(board_spaces)
+
+        
+        
 
 if __name__ == "__main__":
     main()
