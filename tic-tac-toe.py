c23e2b00b6140a901bbf00ec427948bd2ecd4217
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-12 08:53:39 -0800

    created board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,11 +1,24 @@
-def board():
+def board(spots):
+    row1 = [spots[0],"|",spots[1],"|",spots[2]]
+    row2 = ["-----"]
+    row3 = [spots[3],"|",spots[4],"|",spots[5]]
+    row4 = ["-----"]
+    row5 = [spots[6],"|",spots[7],"|",spots[8]]
+    game_board = [row1, row2, row3, row4, row5]
+    for i in range(len(game_board)):
+        for j in range(len(game_board[i])):
+            print(game_board[i][j],end="")
+        print()
 
 def input():
-
+    pass
 def check():
-
+    pass
 def main():
+    board_spaces = []
+    for i in range(1,10):
+        board_spaces.append(str(i))
+    board(board_spaces)
 
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
